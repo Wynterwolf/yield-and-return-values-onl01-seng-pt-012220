47268b2c17458e45f6a23c6d205a3fd0be689c6c
Samantha Jordan <shinyguns@gmail.com>
2020-01-28 00:13:21 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -10,3 +10,13 @@ def hello(array)
 end
 
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+
+def hello(array) #define method
+  i = 0 #counter starter
+  collection = [] #empty array called collection
+  while i < array.length #while there is length in the array
+    collection << yield(array[i]) #push each one into collection
+    i += 1 #add to counter
+  end
+  collection #call the new array
+end
